0160f34731b92e5efcff4b5b9358339c96e45ee2
Jesús <44319321+Yisas12@users.noreply.github.com>
2020-11-30 09:23:50 +0100

    v2

---
+++ asor_repo/Practica2.1/ejercicio1.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+using namespace std;
+
+int main() {
+	if (setuid(2) == -1) {
+		perror("setuid no valido");
+		strerror(-1);
+	}
+	return 1;
+}
+
